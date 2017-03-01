6df6ef871938cc2914360c4dcd5a0fdbdc05b88b
Paulo Souza <paulo.souza@impacta.com.br>
2017-03-01 17:35:19 -0300

    teste conexao com o banco

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,7 +1,22 @@
 module.exports = function(app){
 
 	app.get('/noticias', function(req, res){
-		res.render('noticias/noticias');
+
+		var mysql = require('mysql');
+
+		var connection = mysql.createConnection({
+			host : 'localhost',
+			user : 'root',
+			password : '',
+			datebase : 'noticias'
+		});
+
+		connection.query('SELECT * FROM tb_noticias', function(error, result){
+			res.send(result);
+		});
+
+		//res.render('noticias/noticias');
+
 	});
 
 }
