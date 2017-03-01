d88152aab0c8c99bf340eda286f28257237f53ae
Paulo Souza <paulo.souza@impacta.com.br>
2017-03-01 16:39:24 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var app = require('./config/server');
+
+var rotaNoticias = require('./app/routes/noticias')(app);
+var rotaHome = require('./app/routes/home')(app);
+var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia')(app);
+
+app.listen(3000, function(){
+	console.log('Server Online!');
+});
+
+
+/*
+Instalação de Módulos:
+npm install express -save
+npm install ejs --save
+npm install -g nodemon
+npm install mysql --save
+*/
