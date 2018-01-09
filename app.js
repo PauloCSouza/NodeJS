80b830ed56c70ed476666dfe8ef1694e14713b20
Paulo Souza <paulo.souza@impacta.com.br>
2018-01-09 15:58:25 -0200

    add modulo consign

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var app = require('./config/server');
 
-var rotaNoticias = require('./app/routes/noticias')(app);
-var rotaHome = require('./app/routes/home')(app);
-var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia')(app);
+//var rotaNoticias = require('./app/routes/noticias')(app);
+//var rotaHome = require('./app/routes/home')(app);
+//var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia')(app);
 
 app.listen(3000, function(){
 	console.log('Server Online!');
@@ -15,4 +15,5 @@ npm install express -save
 npm install ejs --save
 npm install -g nodemon
 npm install mysql --save
+npm install consign --save
 */
